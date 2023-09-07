6b5e3d7da86d43f735ccb7758f61d8154ce92be9
ghifari <ghifariakbar2018@gmail.com>
2023-09-07 12:33:36 +0700

    reminder in dmy

--- lib/application/reminder/reminder_notifier.dart
+++ lib/application/reminder/reminder_notifier.dart
@@ -29,15 +29,11 @@ class ReminderNotifier extends StateNotifier<ReminderState> {
   DateTime convertToDateTime({required String passUpdate}) {
     List<String> parts = passUpdate.split(' ');
     List<String> dateParts = parts[0].split('-');
-    List<String> timeParts = parts[1].split(':');
 
     int year = int.parse(dateParts[0]);
     int month = int.parse(dateParts[1]);
     int day = int.parse(dateParts[2]);
-    int hour = int.parse(timeParts[0]);
-    int minute = int.parse(timeParts[1]);
-    int second = int.parse(timeParts[2]);
 
-    return DateTime(year, month, day, hour, minute, second);
+    return DateTime(year, month, day);
   }
 }
