0fcd60e3e715bf1d02798bacddd588f37183fcb6
ghifari <ghifariakbar2018@gmail.com>
2023-10-11 13:43:04 +0700

    imei registered

--- lib/application/imei/imei_notifier.dart
+++ lib/application/imei/imei_notifier.dart
@@ -195,8 +195,8 @@ class ImeiNotifier extends StateNotifier<ImeiState> {
         case true:
           // debugger(message: 'called');
 
-          // await onImeiAlreadyRegistered();
-          onImeiOK();
+          await onImeiAlreadyRegistered();
+          // onImeiOK();
           // await onImeiNotRegistered();
 
           break;
@@ -209,8 +209,8 @@ class ImeiNotifier extends StateNotifier<ImeiState> {
             } else if (imeiDBString != savedImei) {
               debugger(message: 'called');
 
-              // await onImeiAlreadyRegistered();
-              onImeiOK();
+              await onImeiAlreadyRegistered();
+              // onImeiOK();
               // await onImeiNotRegistered();
             }
           }();
