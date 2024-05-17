29204de7f4c5e1d0889d41000015841596e118d4
ghifari <ghifariakbar2018@gmail.com>
2024-05-17 10:47:08 +0700

    fix(upgrader) : added upgrader
    added upgrader on on homepage
    
    fix #9

--- lib/home/presentation/home_page.dart
+++ lib/home/presentation/home_page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:upgrader/upgrader.dart';
 
+import '../../firebase/remote_config/application/firebase_remote_cfg.dart';
+import '../../firebase/remote_config/application/firebase_remote_config_notifier.dart';
 import '../../init/presentation/init_geofence_scaffold.dart';
+import '../../main.dart';
 import '../../network_state/application/network_state_notifier.dart';
+import '../../shared/providers.dart';
+import '../../widgets/v_async_widget.dart';
 
 class HomePage extends ConsumerStatefulWidget {
   const HomePage();
@@ -16,7 +22,23 @@ class _HomePageState extends ConsumerState<HomePage> {
   @override
   Widget build(BuildContext context) {
     ref.listen(networkCallbackProvider, (_, __) {});
+    final router = ref.watch(routerProvider);
+    final firebaseRemoteCfg = ref.watch(firebaseRemoteConfigNotifierProvider);
 
-    return InitGeofenceScaffold();
+    return VAsyncValueWidget<FirebaseRemoteCfg>(
+      value: firebaseRemoteCfg,
+      data: (cfg) => UpgradeAlert(
+        key: UniqueKey(),
+        child: InitGeofenceScaffold(),
+        navigatorKey: router.routerDelegate.navigatorKey,
+        upgrader: Upgrader(
+            minAppVersion: cfg.minApp,
+            durationUntilAlertAgain: Duration(hours: 3),
+            dialogStyle: UpgradeDialogStyle.cupertino,
+            showLater: true,
+            showIgnore: false,
+            messages: MyUpgraderMessages()),
+      ),
+    );
   }
 }
